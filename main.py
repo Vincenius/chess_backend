b2a955a9d02f21626ce7153184fcc8b30cc71434
Vincenius <vincent.will@vaitrade.de>
2021-02-19 15:23:31 +0100

    add post endpoint, accepting image data

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods = ['POST'])
+def new_user():
+    image_data = request.files['input_name']
+    # add here the code to create the user
+
+    print(image_data)
+
+    res = {"status": "ok"}
+    return jsonify(res)
